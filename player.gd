2abc094d3fde72c65e35816f0ad4ec05fbe370b6
calico-dev <vincentodowd@proton.me>
2025-12-11 13:54:27 -0500

    Ledge Climb Boost
    Gave a little boost to ledge climbing for a faster feel

--- player.gd
+++ player.gd
@@ -69,7 +69,7 @@ func _physics_process(delta: float) -> void:
 	else:
 		speed = WALK_SPEED
 
-	# Get the input direction and handle the movement/deceleration.
+	# Get the input direction to handle the movement/deceleration.
 	var input_dir := Input.get_vector("left", "right", "forward", "back")
 	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	
@@ -102,6 +102,6 @@ func _physics_process(delta: float) -> void:
 	# "Climb" up ledge using raycast
 	if above_ledge.is_colliding() == false and below_ledge.is_colliding() == true:
 		if Input.is_action_just_pressed("jump"):
-			velocity.y = JUMP_VELOCITY 
+			velocity.y = JUMP_VELOCITY * 1.1
 	
 	move_and_slide()
